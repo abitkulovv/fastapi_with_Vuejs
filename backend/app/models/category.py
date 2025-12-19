216c57309ba96dcc773c51c1bb31a430f33a72a5
dosya87 <dastanabitkulov901@gmail.com>
2025-12-19 21:50:02 +0600

    add run and main.py and some fixes

--- backend/app/models/category.py
+++ backend/app/models/category.py
@@ -6,7 +6,7 @@ from ..database import Base
 class Category(Base):
     __tablename__ = "categories"
 
-    id = Column(Integer, primery_key=True, index=True)
+    id = Column(Integer, primary_key=True, index=True)
     name = Column(String, unique=True, nullable=False, index=True)
     slug = Column(String, unique=True, nullable=False, index=True)
 
